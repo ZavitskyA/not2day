817532af79adf384b485661f3d5a433f7b4f3ca4
ZavitskyA <Andrejs.Zavickis@gmail.com>
2020-05-07 22:41:09 +0300

    title changed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Probably will create awesome page, but not2day
         </p>
